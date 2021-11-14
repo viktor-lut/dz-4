84c9a1c01e2947a8dfd07015266435f33646887a
viktor lut <lvihard@gmail.com>
2021-11-14 16:36:02 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function summ (a, b) {
+  return a+b;
+}
+
+function mult5 (a, b) {
+  return a*b;
+}
+
+function divis (a, b) {
+  return a/b;
+}
+
+console.log(`summa 2&3 = ${summ(2,3)}`)
+console.log(`multiplicdation 2&3 = ${mult5(2,3)}`)
+console.log(`division 2&3 = ${divis(2,3)}`)
